338af00c6f63f5e01edc34e51451b32da6ca59e6
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-02 13:11:29 -0700

    Update postcss.config.ts and tailwind.config.ts

--- src/frontend/newsfeed/tailwind.config.ts
+++ src/frontend/newsfeed/tailwind.config.ts
@@ -1,16 +1,14 @@
-module.exports = {
-  purge: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@mui/material/**/*.d.ts',
-  ],
-  darkMode: 'class',
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./app/**/*.{js,jsx,ts,tsx}',],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-  corePlugins: {},
+  corePlugins: {
+    preflight: false,
+  },
+  darkMode: 'class',
   prefix: 'tw-',
-}
+} satisfies Config
