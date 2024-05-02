fcd84c34229afef2e0c4f19cdfb81169a51c0816
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-02 12:54:36 -0700

    Add Subscription route component

---
+++ src/frontend/newsfeed/app/routes/subscription.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+
+export default function Subscription() {
+
+    return (
+        <Container>
+            <h1>Subscription</h1>
+        </Container>
+    )
+}
