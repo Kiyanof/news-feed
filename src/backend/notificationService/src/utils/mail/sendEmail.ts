1aee2373c853221dfb7395ed6fc9502dd34731af
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-03 11:44:18 -0700

    chore: Add sendEmail utility function for sending emails

---
+++ src/backend/notificationService/src/utils/mail/sendEmail.ts
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer"
+import EMAIL_CONFIG from "../../config/email.conf"
+import logger from "../../config/logger"
+
+const transporter = nodemailer.createTransport({
+    service: EMAIL_CONFIG.SERVICE,
+    auth: {
+        user: EMAIL_CONFIG.AUTH.USERNAME,
+        pass: EMAIL_CONFIG.AUTH.PASSWORD
+    }
+})
+
+
+const sendEmail = async (to: string, subject: string, text: string) => {
+    logger.debug(`Sending email to ${to}`)
+    const mailOptions = {
+        from: EMAIL_CONFIG.AUTH.USERNAME,
+        to,
+        subject,
+        text
+    }
+    logger.debug(`Mail options: ${JSON.stringify(mailOptions)}`)
+    try {
+        await transporter.sendMail(mailOptions)
+        logger.info(`Email sent to ${to}`)
+        return true
+    } catch (error) {
+        console.error(error)
+        logger.error(`Error happened while sending email to ${to}, Error: ${error}`)
+        return false
+    }
+}
+
+export default sendEmail
