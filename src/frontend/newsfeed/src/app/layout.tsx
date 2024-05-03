3d9c4db1bb8e57f642c655aecf4f07e2dec91a1a
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-03 09:10:16 -0700

    refactor: Update layout component to include Body and StoreProvider

--- src/frontend/newsfeed/src/app/layout.tsx
+++ src/frontend/newsfeed/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Body from "./Body";
+import StoreProvider from "./StoreProvider";
 
 export const metadata: Metadata = {
   title: "News Feed",
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <StoreProvider>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <Body>
+            {children}
+          </Body>
+      </StoreProvider>
     </html>
   );
 }
