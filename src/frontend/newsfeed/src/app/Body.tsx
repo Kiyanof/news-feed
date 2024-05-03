eaf624f81a12bf47c94ec56b96bd3c2608253bab
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-03 09:10:09 -0700

    feat: Add MainNav component for navigation in Body component

--- src/frontend/newsfeed/src/app/Body.tsx
+++ src/frontend/newsfeed/src/app/Body.tsx
@@ -6,27 +6,26 @@ import {
   createTheme,
 } from "@mui/material";
 import { HTMLAttributes, useRef } from "react";
-import { useSelector } from "react-redux";
-
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import StoreProvider from "./StoreProvider";
-import { userToken } from "src/lib/redux/features/app/appSlice";
-import { darkMode } from "src/lib/redux/features/theme/themeSlice";
+import MainNav from "./components/navs/MainNav";
 
 interface BodyProps extends HTMLAttributes<HTMLBodyElement> {}
 
 const Body: React.FC<BodyProps> = ({ className, children, ...props }) => {
-  const uToken = useSelector(userToken);
-  const isDark = useSelector(darkMode);
+  const isDark = false; // TODO: useSelector(darkMode);
 
   const theme = createTheme({
     palette: {
       mode: isDark ? "dark" : "light",
+      // TODO: Change primary and secondary colors
     },
+    typography: {
+      fontFamily: "serif"
+    }
   });
 
+
   return (
-    <StoreProvider>
       <AppRouterCacheProvider options={{
         speedy: true,
         enableCssLayer: true,
@@ -34,17 +33,16 @@ const Body: React.FC<BodyProps> = ({ className, children, ...props }) => {
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={theme}>
           <body className={`${className} ${isDark && "dark"}`} {...props}>
-            {uToken && (
-              <div>
-                
-              </div>
-            )}
-            <main className="p-4 dark:bg-zinc-900">{children}</main>
+            <nav className="h-fit">
+              <MainNav />
+            </nav>
+            <main className="p-4 dark:bg-zinc-900 tw-mt-[66px]">
+              {children}
+            </main>
           </body>
         </ThemeProvider>
       </StyledEngineProvider>
       </AppRouterCacheProvider>
-      </StoreProvider>
   );
 };
 
