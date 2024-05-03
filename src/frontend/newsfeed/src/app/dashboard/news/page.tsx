7c8ef22b6564a4257d14ccaaff02c52b256a678a
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-03 09:12:46 -0700

    Add news page component for displaying news articles

---
+++ src/frontend/newsfeed/src/app/dashboard/news/page.tsx
@@ -0,0 +1,14 @@
+import Newspapers from "@/app/components/layout/Newspapers"
+import { Container } from "@mui/material"
+import exp from "constants"
+
+const Page = () => {
+
+    return (
+        <Container>
+            <Newspapers />
+        </Container>
+    )
+}
+
+export default Page;
