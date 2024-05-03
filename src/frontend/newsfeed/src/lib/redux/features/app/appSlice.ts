a2731d7d21db18365471cd4378587168660d6d13
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-03 09:08:30 -0700

    refactor: Remove unused files and configurations, update Redux store and hooks for state management

--- src/frontend/newsfeed/src/lib/redux/features/app/appSlice.ts
+++ src/frontend/newsfeed/src/lib/redux/features/app/appSlice.ts
@@ -2,26 +2,18 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '../../store';
 
 interface AppState {
-    userToken?: string;
-    notificationCounter?: number;
 }
 
 const initialState: AppState = {
-    userToken: 'undefined',
-    notificationCounter: 0,
 };
 
 const appSlice = createSlice({
     name: "app",
     initialState,
     reducers: {
-        setUserToken: (state, action: PayloadAction<string>) => {
-            state.userToken = action.payload;
-        },
+       
     },
 });
 
-export const { setUserToken } = appSlice.actions;
-export const userToken = (state: RootState) => state.app.userToken;
-export const notificationCounter = (state: RootState) => state.app.notificationCounter;
+export const { } = appSlice.actions;
 export default appSlice.reducer;
