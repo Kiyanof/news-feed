a040787737856647c54c4095194d26f361428586
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-06 08:29:19 -0700

    feat: Add News model for news feed service

---
+++ src/backend/newsFeedService/src/model/news.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+interface INews {
+    [key: string]: any,
+    title: string,
+    description: string,
+    content: string,
+    category: string,
+    author: string,
+    keywords: string,
+    country: string,
+    language: string,
+    source: string,
+    url: string,
+    publishedAt: Date,
+}
+
+const newsSchema = new mongoose.Schema<INews>({
+    title: { type: String, required: true},
+    description: { type: String, required: true},
+    content: { type: String, required: true },
+    category: { type: String, required: false, default: "General" },
+    author: { type: String, required: false, default: "Anonymous" },
+    keywords: { type: String, required: false, default: "" },
+    country: { type: String, required: false, default: "Unknown" },
+    language: { type: String, required: false, default: "English" },
+    source: { type: String, required: false, default: "Unknown"},
+    url: { type: String, required: false, default: ""},
+    publishedAt: { type: Date, required: false, default: Date.now() },
+})
+
+const NewsModel = mongoose.model<INews>('News', newsSchema)
+export default NewsModel;
