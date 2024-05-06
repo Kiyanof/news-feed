558108b8ac0a4dba8b616976157c59afc639cfaf
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-06 08:29:28 -0700

    chore: Add mongo.config.ts for news feed service

---
+++ src/backend/newsFeedService/src/config/mongo.config.ts
@@ -0,0 +1,11 @@
+const MONGO_CONFIG = {
+    PROTOCOL: process.env.MONGO_PROTOCOL || 'mongodb',
+    HOST: process.env.MONGO_HOST || 'localhost',
+    PORT: process.env.MONGO_PORT || '27017',
+    DB: process.env.MONGO_DB || 'newsfeed',
+    USER: process.env.MONGO_USERNAME || 'guest',
+    PASSWORD: process.env.MONGO_PASSWORD || 'guest',
+}
+
+export const MONGO_URI = `${MONGO_CONFIG.PROTOCOL}://${MONGO_CONFIG.HOST}:${MONGO_CONFIG.PORT}/${MONGO_CONFIG.DB}`
+export default MONGO_CONFIG;
