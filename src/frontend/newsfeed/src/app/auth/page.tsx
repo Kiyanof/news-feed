95b4be4e6a5009ac72144f73132eef81e089d38f
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-07 05:24:16 -0700

    feat: Add authentication page with SigninForm component

---
+++ src/frontend/newsfeed/src/app/auth/page.tsx
@@ -0,0 +1,11 @@
+import SigninForm from "../components/forms/signin/SigninForm";
+import SignupForm from "../components/forms/signup/SignupForm";
+
+const Page = () => {
+
+    return (
+        <SigninForm />
+    )
+}
+
+export default Page;
