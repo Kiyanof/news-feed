c4ba68d8c347ae54d21e58c6dfea59bfa67d0f4f
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-10 00:37:01 -0700

    chore: Update rabbitmq package version and add logger configuration

--- src/backend/modules/rabbitmq/index.ts
+++ src/backend/modules/rabbitmq/index.ts
@@ -1,150 +1,178 @@
-import amqp, { Channel, Message } from "amqplib"
+import amqp, { Channel, Message } from "amqplib";
 import createLogger from "../logger";
+import createProducer, { ProducerType } from "./rpc/producer";
+import createConsumer, { ConsumerType } from "./rpc/consumer";
 
-const logger = createLogger("rabbitmq-service")
+const logger = createLogger("rabbitmq-service");
 
 class Rabbit {
+  private static _instance: Rabbit | null = null;
+  private _connection: amqp.Connection | null = null;
+  private _channel: amqp.Channel | null = null;
 
-    private static _instance: Rabbit | null = null;
-    private _connection: amqp.Connection | null = null;
-    private _channel: amqp.Channel | null = null;
+  private _max_attemp: number = 10;
 
-    private _max_attemp: number = 10
+  public static new({ ...props }) {
+    logger.info(`Rabbit: Creating a new instance...`);
+    if (!Rabbit._instance) return new Rabbit({ ...props });
+    return Rabbit._instance;
+  }
 
-    public static new({...props}){
-        logger.debug(` `)
-        if(!Rabbit._instance) return new Rabbit(props);
-        return Rabbit._instance;
-    }
+  private constructor({ ...props }) {
+    logger.info(`A new rabbit instance has been created.`);
+    logger.info(`url: ${props.url}`);
+    this.connect(props).then(async () => {
+      logger.debug(`Rabbit: connection established.`);
+      logger.debug(`Rabbit: channel creating...`);
+      try {
+        this._channel = await this.createChannel();
+        logger.info(`Rabbit: Channel created successfully`);
+      } catch (error) {
+        logger.error(`Rabbit: Error while creating channel, Error: ${error}`);
+      }
+    });
+  }
 
-    private constructor({...props}){
-        logger.debug(`A new rabbit instance has been created.`)
-        this.connect(props).then(async () => {
-            logger.debug(`Rabbit: connection established.`)
-            logger.debug(`Rabbit: channel creating...`)
-            try {
-                this._channel = await this.createChannel()
-                logger.info(`Rabbit: Channel created successfully`)
-            } catch (error) {
-                logger.error(`Rabbit: Error while creating channel, Error: ${error}`)
-            }            
-        })
-    }
+  private static response(
+    channel: Channel,
+    req: Message,
+    res: { state: boolean; msg: string; body: any }
+  ) {
+    channel.sendToQueue(
+      req.properties.replyTo,
+      Buffer.from(JSON.stringify(res)),
+      { correlationId: req.properties.correlationId }
+    );
+    channel.ack(req);
+  }
 
-    private static response(channel: Channel, req: Message, res: {state: boolean, msg: string, body: any}) {
-        channel.sendToQueue(req.properties.replyTo, Buffer.from(JSON.stringify(res)), {correlationId: req.properties.correlationId})
-        channel.ack(req)
-    }
-    
-    public static handleSuccess (channel: Channel, req: Message, msg: string, body: any) {
-      const result = {
-        state: true,
-        msg,
-        body,
-      };
-      logger.info(msg);
-      this.response(channel, req, result)
+  public static handleSuccess(
+    channel: Channel,
+    req: Message,
+    msg: string,
+    body: any
+  ) {
+    const result = {
+      state: true,
+      msg,
+      body,
     };
-    
-    public static handleError(channel: Channel, req: Message, msg: string, body: any) {
-      const result = {
-        state: false,
-        msg,
-        body,
-      };
-      logger.error(msg + ' with error: ' + body);
-      this.response(channel, req, result)
+    logger.info(msg);
+    this.response(channel, req, result);
+  }
+
+  public static handleError(
+    channel: Channel,
+    req: Message,
+    msg: string,
+    body: any
+  ) {
+    const result = {
+      state: false,
+      msg,
+      body,
     };
-    
-    
-    public static initQueue (channel: Channel, queue: string, prefetchCount: number) {
-        channel.assertQueue(queue, {
-            durable: false,
-          });
-        channel.prefetch(prefetchCount); 
-        logger.info(
-            `RABBITMQ: ${JSON.stringify(channel)} channel Awaiting RPC requests... with prefetch: ${prefetchCount}`
-          );
-    }
+    logger.error(msg + " with error: " + body);
+    this.response(channel, req, result);
+  }
 
-    private async connect({...props}) {
-        const url = `${props.protocol ?? 'amqp'}://${props.username ?? 'guest'}:${props.password ?? 'guest'}@${props.host ?? 'localhost'}:${props.port ?? 5672}/${props.vhost}`
-        logger.debug(`Connecting to RabbitMQ at ${url}`)
-        try {
-            this._connection = await amqp.connect(url)
-            logger.info(`Connected to RabbitMQ at ${url}`)
-        } catch (error) {
-            logger.error(`Failed to connect to RabbitMQ at ${url}`)
-            this._connection = null
+  public static initQueue(
+    channel: Channel,
+    queue: string,
+    prefetchCount: number
+  ) {
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+    channel.prefetch(prefetchCount);
+    logger.info(
+      `RABBITMQ: ${channel} channel Awaiting RPC requests... with prefetch: ${prefetchCount}`
+    );
+  }
+
+  private async connect({ ...props }) {
+    const url =
+      props.url ??
+      `${props.protocol ?? "amqp"}://${props.username ?? "guest"}:${
+        props.password ?? "guest"
+      }@${props.host ?? "localhost"}:${props.port ?? 5672}/${props.vhost}`;
+    logger.debug(`Connecting to RabbitMQ at ${url}`);
+    try {
+      this._connection = await amqp.connect(url);
+      logger.info(`Connected to RabbitMQ at ${url}`);
+    } catch (error) {
+      logger.error(`Failed to connect to RabbitMQ at ${url}`);
+      this._connection = null;
+    }
+  }
+
+  private async createChannel() {
+    if (!this._connection) return null;
+    return await this._connection.createChannel();
+  }
+
+  public isReady() {
+    return new Promise((resolve, _reject) => {
+      let i = 0;
+      const checkReady = () => {
+        i++;
+        if (i < this._max_attemp) {
+          logger.debug(`Rabbit: ${i} attemp to checking channel exist`);
+          if (this._channel) {
+            logger.info(`Rabbit: channel is ready`);
+            return resolve(true);
+          } else {
+            logger.warn(`Rabbit: channel is not ready...`);
+          }
+
+          setTimeout(checkReady, 500);
+        } else {
+          logger.warn(`Rabbit: channel not!!`);
+          resolve(false);
         }
+      };
+
+      checkReady();
+    });
+  }
+
+  public async callProcedure(
+    cb: (channel: Channel, { ...props }) => Promise<any>,
+    { ...props }
+  ) {
+    if (this._channel) {
+      logger.debug(`Rabbit: Processing a procedure call.`);
+      try {
+        const result = await cb(this._channel, props);
+        logger.debug(`Rabbit: CB result = ${JSON.stringify(result)}`);
+        if (result.state) {
+          logger.info(`Rabbit: ${cb.name} procedure called successfully`);
+          return result;
+        } else throw new Error(`consumer send me error`);
+      } catch (error) {
+        logger.error(`Rabbit: ${cb.name} procudure error happened: ${error}`);
+      }
+    } else logger.warn(`Rabbit: channel not exist!`);
+  }
+
+  public addToQueue(consumer: (channel: Channel) => any) {
+    if (this._channel) {
+      consumer(this._channel);
+    } else logger.warn(`Rabbit: channel not exist!`);
+  }
+
+  destroy() {
+    try {
+      this._channel?.close();
+      this._connection?.close();
+    } catch (error) {
+      logger.error(
+        `Rabbit: error happened while RabbitMQ Instance destructing..., Error: ${error} `
+      );
     }
-
-    private async createChannel() {
-        if(!this._connection) return null
-        return await this._connection.createChannel()
-    }
-
-    public isReady() {
-        return new Promise((resolve, _reject) => {
-            let i = 0;
-            const checkReady = () => {
-                i++;
-                if(i < this._max_attemp) {
-                    logger.debug(`Rabbit: ${i} attemp to checking channel exist`)
-                    if(this._channel) {
-                        logger.info(`Rabbit: channel is ready`)
-                        return resolve(true)
-                    } else {
-                        logger.warn(`Rabbit: channel is not ready...`)
-                    }
-
-                    setTimeout(checkReady, 500)
-                } else {
-                    logger.warn(`Rabbit: channel not!!`)
-                    resolve(false)
-                }
-            }
-
-            checkReady()
-        })
-    }
-
-    public async callProcedure(cb: (channel: Channel, {...props}) => Promise<any>, {...props}) {
-        if(this._channel){
-            logger.debug(`Rabbit: Processing a procedure call.`)
-            try {
-                const result = await cb(this._channel, props)
-                logger.debug(`Rabbit: CB result = ${JSON.stringify(result)}`)
-                if(result.state){
-                    logger.info(`Rabbit: ${cb.name} procedure called successfully`)
-                    return result
-                }
-                else throw new Error(`consumer send me error`)
-            } catch (error) {
-                logger.error(`Rabbit: ${cb.name} procudure error happened: ${error}`)
-            }
-        }
-        else logger.warn(`Rabbit: channel not exist!`)
-    }
-
-    public addToQueue(consumer: (channel: Channel) => any) {
-        if(this._channel){
-            consumer(this._channel)
-        }
-        else logger.warn(`Rabbit: channel not exist!`)
-    }
-
-    destroy() {
-        try {
-            this._channel?.close()
-            this._connection?.close()
-        } catch (error) {
-            logger.error(`Rabbit: error happened while RabbitMQ Instance destructing..., Error: ${error} `)
-        }
-        Rabbit._instance = null
-    }
-
+    Rabbit._instance = null;
+  }
 }
 
-export { Channel }
-export default Rabbit
+export { Channel, ProducerType, ConsumerType, createProducer, createConsumer };
+export default Rabbit;
---
+++ src/backend/modules/rabbitmq/rpc/consumer.ts
@@ -0,0 +1,52 @@
+import { Channel } from "amqplib";
+import Rabbitmq from "../index";
+
+export interface ConsumerType {
+  procedureName: string;
+  defaultQueue: string;
+  prefetch?: number;
+  callback: (props: any) => Promise<any>;
+}
+
+const createConsumer = ({ ...props }: ConsumerType) => {
+  return (channel: Channel): void => {
+    const queue = props.defaultQueue;
+    const prefetchCount = props.prefetch || 1;
+
+    Rabbitmq.initQueue(channel, queue, prefetchCount);
+
+    channel.consume(queue, async (msg) => {
+      if (msg) {
+        try {
+          const content = JSON.parse(msg.content.toString());
+          const isSuccessful = await props.callback(JSON.parse(content));
+
+          if (isSuccessful) {
+            Rabbitmq.handleSuccess(
+              channel,
+              msg,
+              `${props.procedureName} replied successfully to user`,
+              ""
+            );
+          } else {
+            Rabbitmq.handleError(
+              channel,
+              msg,
+              `${props.procedureName} replied failed to user`,
+              ""
+            );
+          }
+        } catch (error) {
+          Rabbitmq.handleError(
+            channel,
+            msg,
+            `${props.procedureName} sent failed to user`,
+            error
+          );
+        }
+      }
+    });
+  };
+};
+
+export default createConsumer;
