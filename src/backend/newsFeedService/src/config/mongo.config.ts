1449104c94a289b5217c219e41086b9f29ed1031
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-13 05:18:16 -0700

    refactor: Update NEWS_EXPIRE configuration in mongo.config.ts

--- src/backend/newsFeedService/src/config/mongo.config.ts
+++ src/backend/newsFeedService/src/config/mongo.config.ts
@@ -5,6 +5,8 @@ const MONGO_CONFIG = {
     DB: process.env.MONGO_DB || 'newsfeed',
     USER: process.env.MONGO_USERNAME || 'guest',
     PASSWORD: process.env.MONGO_PASSWORD || 'guest',
+
+    NEWS_EXPIRE: process.env.NEWS_EXPIRE || 60 * 60 * 24 * 30, // 30 days
 }
 
 export const MONGO_URI = `${MONGO_CONFIG.PROTOCOL}://${MONGO_CONFIG.HOST}:${MONGO_CONFIG.PORT}/${MONGO_CONFIG.DB}`
