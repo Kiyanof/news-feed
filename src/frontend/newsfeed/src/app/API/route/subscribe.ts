e5abd8ec96e4054fae4bd9a8922ef4a4fc6709a2
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-13 01:24:12 -0700

    refactor: Update subscribe.ts to include fingerprinting for signup and subscription changes

--- src/frontend/newsfeed/src/app/API/route/subscribe.ts
+++ src/frontend/newsfeed/src/app/API/route/subscribe.ts
@@ -1,20 +1,53 @@
 import { SubscribeModel } from "../model/subscribe";
 import { SUBSCRIBE_CONFIG } from "../conf/subscribe.conf";
 import { URLGenerator } from "../conf/main.conf";
-
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
 const subscribe = async ({ ...props }: SubscribeModel) => {
   try {
+    const fingerprint = await (await FingerprintJS.load()).get();
+
     const result = await SUBSCRIBE_CONFIG.subscribe.method(
-      URLGenerator(SUBSCRIBE_CONFIG.subscribe.endpoint, ""),
-      {
-          body: JSON.stringify(props),
-      }
+      URLGenerator(SUBSCRIBE_CONFIG.subscribe.endpoint, SUBSCRIBE_CONFIG.subscribe.path),
+      {...props, fingerPrint: fingerprint.visitorId},
+      { withCredentials: true}
     );
-    if(result.status !== 200) return null;
+    if(result.status !== 201) return {
+      message: "Signup failed",
+      error: [],
+      data: null,
+    };
     return result.data;
   } catch (error) {
-    return null
+    return {
+      message: `Application error`,
+      error: [error],
+      data: null,
+    }
   }
 };
 
-export { subscribe };
+const changeSubscription = async ({ ...props }: SubscribeModel) => {
+  try {
+    const fingerprint = await (await FingerprintJS.load()).get();
+
+    const result = await SUBSCRIBE_CONFIG.changeSubscription.method(
+      URLGenerator(SUBSCRIBE_CONFIG.changeSubscription.endpoint, SUBSCRIBE_CONFIG.changeSubscription.path),
+      {...props, fingerPrint: fingerprint.visitorId},
+      { withCredentials: true}
+    );
+    if(result.status !== 201) return {
+      message: "Change subscription failed",
+      error: [],
+      data: null,
+    };
+    return result.data;
+  } catch (error) {
+    return {
+      message: `Application error`,
+      error: [error],
+      data: null,
+    }
+  }
+}
+
+export { subscribe, changeSubscription };
