40b45a2739e70e3517149b314fd22fdac6eaa680
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-13 01:39:31 -0700

    refactor: Add logger and RabbitMQ configuration files

---
+++ src/backend/subscriptionService/src/config/rabbit.conf.ts
@@ -0,0 +1,16 @@
+const RABBIT_CONFIG = {
+    PROTOCOL: process.env.BROKER_PROTOCOL || 'amqp',
+    HOST: process.env.BROKER_HOST || 'localhost',
+    PORT: process.env.BROKER_PORT || 5672,
+    
+    DEFAULT_VHOST: {
+        NAME: process.env.BROKER_DEFAULT_VHOST || 'subscriptionVhost',
+        CREDENTIAL: {
+            USERNAME: process.env.BROKER_DEFAULT_USERNAME ||'guest',
+            PASSWORD: process.env.BROKER_DEFAULT_PASSWORD || 'guest'
+        }
+    }
+}
+
+export const RABBIT_URL = `${RABBIT_CONFIG.PROTOCOL}://${RABBIT_CONFIG.DEFAULT_VHOST.CREDENTIAL.USERNAME}:${RABBIT_CONFIG.DEFAULT_VHOST.CREDENTIAL.PASSWORD}@${RABBIT_CONFIG.HOST}:${RABBIT_CONFIG.PORT}/${RABBIT_CONFIG.DEFAULT_VHOST.NAME}`
+export default RABBIT_CONFIG;
