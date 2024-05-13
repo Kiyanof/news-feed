8a0ce58a3bcaca14772caf2f21d66c5d3217ceb6
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-13 05:13:19 -0700

    refactor: Add rate limiting to authentication routes

--- src/backend/userService/src/route/auth.ts
+++ src/backend/userService/src/route/auth.ts
@@ -2,14 +2,24 @@ import {Router} from 'express'
 import { inputValidation as signupInputValidation, isUserExist } from '../middleware/signup'
 import { login, logout, signup, whoIsMe } from '../controller/auth'
 import { logRequests } from '../middleware/request'
+import rateLimit from 'express-rate-limit';
 
 const router = Router()
 
+// Define rate limit rule
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again later."
+  });
+
 router.use(logRequests)
+router.use(limiter)
 
 router.post('/subscribe', signupInputValidation, isUserExist, signup)
-router.post('/whoisme', whoIsMe)
 router.post('/login', login)
 router.post('/logout', logout)
+
+router.post('/whoisme', whoIsMe) // Becareful about limiting this 
 
 export default router
