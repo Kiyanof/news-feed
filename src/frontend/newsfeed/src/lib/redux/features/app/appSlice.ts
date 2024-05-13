b49743e6b8799f291511a23ece1b4afb49e5a301
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-13 01:21:50 -0700

    feat: Add setUser and clearUser actions to appSlice.ts

--- src/frontend/newsfeed/src/lib/redux/features/app/appSlice.ts
+++ src/frontend/newsfeed/src/lib/redux/features/app/appSlice.ts
@@ -2,18 +2,26 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '../../store';
 
 interface AppState {
+    user: string | null;
 }
 
 const initialState: AppState = {
+    user: null
 };
 
 const appSlice = createSlice({
     name: "app",
     initialState,
     reducers: {
-       
+        setUser: (state, action: PayloadAction<string>) => {
+            state.user = action.payload;
+        },
+        clearUser: (state) => {
+            state.user = null;
+        }
     },
 });
 
-export const { } = appSlice.actions;
+export const { setUser, clearUser } = appSlice.actions;
+export const appUser = (state: RootState) => state.app.user;
 export default appSlice.reducer;
