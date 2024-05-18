c63f2335ecdfe3249f9dc457b0c2f691656b5fda
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2024-05-18 01:44:24 -0700

    refactor: update procedure and queue names in getKeywordsProducer

--- src/backend/modules/producers/subscription/producers/getKeywords.ts
+++ src/backend/modules/producers/subscription/producers/getKeywords.ts
@@ -2,8 +2,8 @@ import {Channel, createProducer} from 'rabbitmq'
 
 const getKeywordsProducer = async (channel: Channel, content: {}, callback?: (content: Object) => Promise<any>) => {
     const producer = createProducer({
-        proceduerName: 'news/getKeywords',
-        defaultQueue: 'getKeywords'
+        proceduerName: 'subscription/readKeywords',
+        defaultQueue: 'readKeywords'
     })
 
     return producer(channel, content, callback)
